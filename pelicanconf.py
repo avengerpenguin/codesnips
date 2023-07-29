03b59775cda6e9dbc69c9ec8ada074f9011fc7e6
Ross Fenning <github@rossfenning.co.uk>
2023-07-29 15:07:47 +0100

    vault backup: 2023-07-29 15:07:47

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,6 +21,13 @@ STATIC_PATHS = [
 ]
 EXTRA_PATH_METADATA = {
     "../extra/CNAME": {"path": "CNAME"},
+    "../extra/android-chrome-512x512.png": {"path": "android-chrome-512x512.png"},
+    "../extra/android-chrome-192x192.png": {"path": "android-chrome-192x192.png"},
+    "../extra/apple-touch-icon.png": {"path": "apple-touch-icon.png"},
+    "../extra/favicon-16x16.png": {"path": "favicon-16x16.png"},
+    "../extra/favicon-32x32.png": {"path": "favicon-32x32.png"},
+    "../extra/favicon.ico": {"path": "favicon.ico"},
+    "../extra/site.webmanifest": {"path": "site.webmanifest"},
 }
 
 WEBASSETS_CONFIG = [
